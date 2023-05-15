900fa9be0f3fbc49bdbf00e407def0b27824cbe5
Venkatrushi Vanga <venkatrushivanga007@gmail.com>
2023-05-16 00:46:30 +0530

    Configration in Controller Example

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -8,19 +8,21 @@ namespace TweetBook.Controllers;
 
 [ApiController]
 public class PostsController {
+    private readonly IConfiguration _configuration;
     private List<Post> _posts;
-
     public PostsController(IConfiguration configuration) {
         _posts = new List<Post>();
         for (var i = 0; i < 5; i++) {
             _posts.Add(new Post { Id = Guid.NewGuid().ToString() });
         }
 
-
+        this._configuration = configuration;
     }
 
     [HttpGet(ApiRoutes.Posts.GetAll)]
     public IActionResult GetPosts() {
-        return new OkObjectResult(_posts);
+        var swaggerOptions = new SwaggerOptions();
+        _configuration.GetSection("SwaggerOptions").Bind(swaggerOptions);
+        return new OkObjectResult(swaggerOptions);
     }
 }
